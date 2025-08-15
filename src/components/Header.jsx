c24e7e11fd90055e994c64e4cd8f2c3deecba839
MartinSJosefsson <martin.s.josefsson@gmail.com>
2025-08-15 13:54:47 +0200

    Header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
    return (
-  <header className="bg-light p-3 border-bottom">
-      <div className="d-flex align-items-center">
-        <h2 className="me-auto"></h2>
+  <header className="container bg-light p-3 border-bottom">
+      <div className="d-flex align-items-center ps-5 pe-3">
+        <h2 className="me-auto">Tasks</h2>
         <div className="input-group w-50 mx-auto">
           <input
             type="text"
